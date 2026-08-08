2d77e547260d5f5d8abe68d901e7c47ea6a0ccfc
aswinscbe <aswincbe301096@gmail.com>
2026-08-09 03:03:20 +0530

    Stabilize mobile asset loading: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-﻿const CACHE="classbl07-final-stable-20260809-v1";
-const SHELL=["./index.html","./styles.css","./visual.css","./app.js","./polish.js","./campus-data.js","./manifest.webmanifest"];
+﻿const CACHE="classbl07-final-stable-20260809-mobile1";
+const SHELL=["./index.html","./styles.css?v=20260809-mobile1","./visual.css?v=20260809-mobile1","./app.js?v=20260809-mobile1","./polish.js?v=20260809-mobile1","./campus-data.js?v=20260809-mobile1","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
