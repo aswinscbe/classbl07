56ac8ddf0732485e00a1fbae9eb45cbdc98194f2
aswinscbe <aswincbe301096@gmail.com>
2026-08-09 02:55:07 +0530

    Restore approved preview: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="classbl07-final-preview-v2";
+﻿const CACHE="classbl07-final-stable-20260809-v1";
 const SHELL=["./index.html","./styles.css","./visual.css","./app.js","./polish.js","./campus-data.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
