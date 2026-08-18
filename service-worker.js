e903e8a48896f1273c19ef414bf6543c872d5c78
aswinscbe <aswincbe301096@gmail.com>
2026-08-18 12:26:14 +0530

    Refine compact dashboard: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE="classbl07-review-20260809-1";
-const CACHE_VERSION="20260811-6";
+const CACHE_VERSION="20260818-29";
 const ACTIVE_CACHE=`${CACHE}-${CACHE_VERSION}`;
-const SHELL=["./index.html","./styles.css?v=20260811-focus6","./visual.css?v=20260811-focus6","./app.js?v=20260811-focus6","./polish.js?v=20260811-focus6","./campus-data.js?v=20260811-focus6","./manifest.webmanifest"];
+const SHELL=["./index.html","./styles.css?v=20260818-focus29","./visual.css?v=20260818-focus29","./app.js?v=20260818-focus29","./polish.js?v=20260818-focus29","./campus-data.js?v=20260818-focus29","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(ACTIVE_CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==ACTIVE_CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
