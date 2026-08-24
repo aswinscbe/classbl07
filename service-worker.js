6ad5bb92406884f155c4413b38074647516fa03d
aswinscbe <aswincbe301096@gmail.com>
2026-08-25 01:32:16 +0530

    Simplify hero hierarchy and stabilize overlays

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE="classbl07-review-20260809-1";
-const CACHE_VERSION="20260825-hero2";
+const CACHE_VERSION="20260825-hero4";
 const ACTIVE_CACHE=`${CACHE}-${CACHE_VERSION}`;
-const SHELL=["./index.html","./styles.css?v=20260825-hero2","./visual-base.css?v=20260825-hero2","./visual.css?v=20260825-hero2","./planner-final.css?v=20260825-hero2","./app.js?v=20260825-hero2","./polish.js?v=20260825-hero2","./campus-data.js?v=20260825-hero2","./exam-data.js?v=20260825-hero2","./brand-mark.svg?v=20260825-hero2","./manifest.webmanifest"];
+const SHELL=["./index.html","./styles.css?v=20260825-hero4","./visual-base.css?v=20260825-hero4","./visual.css?v=20260825-hero4","./planner-final.css?v=20260825-hero4","./app.js?v=20260825-hero4","./polish.js?v=20260825-hero4","./campus-data.js?v=20260825-hero4","./exam-data.js?v=20260825-hero4","./brand-mark.svg?v=20260825-hero4","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(ACTIVE_CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==ACTIVE_CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
