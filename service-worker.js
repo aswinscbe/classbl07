4985e3b0af57f6d1de4b8d722963c0d0f0bb0543
aswinscbe <aswincbe301096@gmail.com>
2026-08-25 17:18:12 +0530

    Fix wide desktop header overlap

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE="classbl07-review-20260809-1";
-const CACHE_VERSION="20260825-system6";
+const CACHE_VERSION="20260825-system7";
 const ACTIVE_CACHE=`${CACHE}-${CACHE_VERSION}`;
-const SHELL=["./index.html","./styles.css?v=20260825-system6","./visual-base.css?v=20260825-system6","./visual.css?v=20260825-system6","./planner-final.css?v=20260825-system6","./premium-system.css?v=20260825-system6","./app.js?v=20260825-system6","./polish.js?v=20260825-system6","./campus-data.js?v=20260825-system6","./exam-data.js?v=20260825-system6","./brand-mark.svg?v=20260825-system6","./manifest.webmanifest"];
+const SHELL=["./index.html","./styles.css?v=20260825-system7","./visual-base.css?v=20260825-system7","./visual.css?v=20260825-system7","./planner-final.css?v=20260825-system7","./premium-system.css?v=20260825-system7","./app.js?v=20260825-system7","./polish.js?v=20260825-system7","./campus-data.js?v=20260825-system7","./exam-data.js?v=20260825-system7","./brand-mark.svg?v=20260825-system7","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(ACTIVE_CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==ACTIVE_CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
